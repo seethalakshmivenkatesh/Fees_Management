dbd0a55461aeada9af7dd6b18d5567be415921fb
seethalakshmivenkatesh <seethavenkatesh79@gmail.com>
2025-10-23 22:06:31 +0530

    initial home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+// import { BrowserRouter, Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./Home";
 function App() {
-  return <div className="bg-amber-400">App</div>;
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>hlo</div>;
+}
+
+export default Home;
